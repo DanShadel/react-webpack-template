00c339daa2fcd0e9d46e41de03bb26d88f3b0752
Dan Shadel <shadeldaniel@gmail.com>
2021-11-26 21:38:29 -0600

    updated app.jsx -> App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components'
 import { Provider } from 'react-redux';
 import store from '../main.js';
-import { Navbar } from './Navbar.jsx';
-import { Intro } from './Intro.jsx';
+import Navbar from './Navbar.jsx';
+import Intro from './Intro.jsx';
 
 const Container = styled.div`
 	width: 100vw;
@@ -12,15 +12,10 @@ const Container = styled.div`
 `;
 
 const App = () => {
-    // React.useEffect((props) => {
-    //     console.log('sup');
-    //     console.log(props);
-    // }, [])
-
     return (
         <Provider store={store}>
             <Container>
-                <Intro />
+                <Navbar />
             </Container>
         </Provider >
     );
