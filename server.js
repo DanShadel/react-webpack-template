fee0814cbd11d8e019a133ec6b97d88d017ae9ba
John Shadel <shadeldaniel@gmail.com>
2021-12-25 23:07:47 -0500

    express routing config

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ let app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
+app.get('*', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'dist')})
+})
+
 const server = app.listen(app.get('port'), () => {
     console.log('listening on port' + server.address().port);
 });
